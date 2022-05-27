c639f616a9220de42dcc7eb7369853749033ed50
aamekst <mieko.alana@gmail.com>
2022-05-26 22:37:56 -0300

    pull

--- Controllers/DisputasController.cs
+++ Controllers/DisputasController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RpgApi.Controllers
 {
@@ -62,15 +64,263 @@ namespace RpgApi.Controllers
                   _context.SaveChanges();
 
 
-
-
-
-
-
-
-
-
                 return Ok(d);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("Habilidade")]
+    public async Task<IActionResult> AtaqueComHabilidadesAsync(Disputa d)
+    {
+        try{
+            //programação dos proximos passos aqui
+            personagem atacante = await _context.personagens
+            .Include(p => p.PersonagemHabilidade).ThenInclude(ph => ph.Habilidade)
+            .FirstOrDefaultAsync(p => p.Id == d.AtacanteId);
+
+            personagem oponente = await _context.personagens
+            .FirstOrDefaultAsync(p => p.Id == d.OponenteId);
+
+            PersonagemHabilidade ph = await _context.PersonagemHabilidades
+            .Include(p => p.Habilidade)
+            .FirstOrDefaultAsync(phBusca => phBusca.HabilidadeId == d.HabilidadeId);
+            
+            if (ph == null)
+            d.Narracao = $"{atacante.Nome} não possui está habilidade";
+            
+            else
+            {
+                int dano = ph.Habilidade.Dano + (new Random().Next(atacante.Inteligencia));
+                dano = dano - new Random().Next(oponente.Defesa);
+
+
+
+                if(dano > 0)
+                oponente.Pontosvida = oponente.Pontosvida - dano;
+
+                if(oponente.Pontosvida <=0)
+                d.Narracao += $"{oponente.Nome} foi derrotado!";
+
+                _context.personagens.Update(oponente);
+                await _context.SaveChangesAsync();
+
+                 StringBuilder dados = new StringBuilder();
+                dados.AppendFormat(" Atacante: {0}. ", atacante.Nome);
+                dados.AppendFormat(" Oponente: {0}. ", oponente.Nome);
+                dados.AppendFormat(" Pontos de vida do atacante: {0}. ", atacante.Pontosvida);
+                dados.AppendFormat(" Pontos de vida do oponente: {0}. ", oponente.Pontosvida);
+                dados.AppendFormat(" Arma utilizada: {0}", ph.Habilidade.Nome);
+                dados.AppendFormat(" Dano: {0}", dano);
+                
+                d.Narracao += dados.ToString();
+                d.DataDisputa = DateTime.Now;
+                _context.Disputas.Add(d);
+                _context.SaveChanges();
+ 
+            }
+
+            return Ok(d);
+        }
+        catch(System.Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }    
+
+
+    [HttpGet("PersonagemRandom")]  
+
+    public async Task<IActionResult> Sorteio()
+    {
+        List<personagem> personagens =
+        await _context.personagens.ToListAsync();
+        
+        //Sorteio com numero da quantidade de personagens
+        int sorteio = new Random().Next(personagens.Count);
+
+        //busca na lista pelo indice  sorteado (Não é o Id)
+        personagem p = personagens[sorteio];
+
+        string msg =
+        string.Format("Nº Sorteado {0}. Personagem: {1}", sorteio, p.Nome);
+
+        return Ok(msg);
+        
+    }  
+
+
+    [HttpGet("DisputaEmGrupo")]
+
+    public async Task<IActionResult> DisputaEmGrupo (Disputa d)
+
+    {
+        try
+        {
+            //Busca na base dos personagens informados no parametro incluindo Armas e Habilidades
+            List<personagem> personagens = await _context.personagens
+            .Include(p => p.Arma)
+            .Include(p => p.PersonagemHabilidade).ThenInclude(ph => ph.Habilidade)
+            .Where(p => d.ListaIdPersonagens.Contains(p.Id)).ToListAsync();
+
+
+            //Contagem de personagens vivos na lista obtida do banco de dados 
+             int qtdPersonagensVivos = personagens.FindAll(p => p.Pontosvida > 0).Count;
+
+
+
+             //Enquanto houver mais de um personagem vivo haverá disputa
+             while (qtdPersonagensVivos > 1)
+             {
+                 //ATENÇÃO: todas as etapas a seguir dever ficar aqui dentro do while
+              //Seleciona personagens com pontos vida positivos e depois faz sorteio.
+
+
+             List<personagem> atacantes = personagens.Where(p => p.Pontosvida > 0).ToList();
+             personagem atacante = atacantes[new Random().Next(atacantes.Count)];
+             d.AtacanteId = atacante.Id;
+
+
+            //Seleciona personagens com pontos vida positivos, exceto o atacante escolhido e depois faz sorteio.
+            List<personagem> oponentes = personagens.Where(p => p.Id != atacante.Id && p.Pontosvida > 0).ToList();
+            personagem oponente = oponentes[new Random().Next(oponentes.Count)];
+            d.OponenteId = oponente.Id;
+
+
+            //declara e redefine a cada passagem do while o valor das variáveis que serão usadas.
+           int dano = 0;
+           string ataqueUsado = string.Empty;
+           string resultado = string.Empty;
+
+           //Sorteia entre O e 1: O é umataque com arma e 1 é um ataque com habilidades
+            bool ataqueUsaArma = (new Random().Next(1) == 0);
+
+            if (ataqueUsaArma && atacante.Arma != null)
+            {
+                //programação do ataque com arma
+               //Sorteio da Força
+               
+                dano = atacante.Arma.Dano + (new Random().Next(atacante.Forca));
+                dano = dano - new Random().Next(oponente.Defesa); //Sorteio da defesa
+                ataqueUsado = atacante.Arma.Nome;
+
+                if (dano > 0)
+                oponente.Pontosvida = oponente.Pontosvida - (int)dano;
+
+                //formata a mensagem 
+
+                resultado =
+                string.Format("{0} atacou {1} usado {2} com dano {3}." , atacante.Nome, oponente.Nome, ataqueUsado, dano);
+                d.Narracao += resultado; // Concatena o resultado com as narrações existentes.
+                d. Resultados .Add(resultado);//Adiciona o resulta atual na lista de resultados.
+
+
+
+            } 
+
+           else if (atacante.PersonagemHabilidade.Count != 0)//Verifica se o personagem tem habilidades
+            {
+            //Programação do ataque com habilidade
+           
+                //Programação do ataque com habilidade
+
+                //Realiza o sorteio entre as habilidade existentes e na linha seguinte a seleciona.
+
+                int sorteioHabilidadeld = new Random().Next(atacante. PersonagemHabilidade.Count);
+                Habilidade habilidadeEscolhida = atacante.PersonagemHabilidade [sorteioHabilidadeld].Habilidade;
+                ataqueUsado = habilidadeEscolhida.Nome;
+
+
+                //Sorteio da inteligência somada ao dano
+
+                dano = habilidadeEscolhida.Dano + (new Random().Next(atacante. Inteligencia));
+                dano = dano - new Random() .Next (oponente.Defesa);//Sorteio da defesa.
+
+                if (dano > 0)
+
+                oponente.Pontosvida = oponente.Pontosvida - (int)dano;
+
+
+                resultado = 
+                string.Format("{0} atacou {1} usado {2} com dano {3}." , atacante.Nome, oponente.Nome, ataqueUsado, dano);
+
+                d.Narracao += resultado;
+                d. Resultados .Add(resultado);
+
+
+                        }
+        //ATENÇÃO aqui ficará a programação da verificação do ataque usado e verificação se existen mais de um personagem vivo
+           if (!string.IsNullOrEmpty(ataqueUsado))
+
+                { //Incrementando os dados dos combates
+                    atacante.Vitorias++;
+                    oponente.Vitorias++;
+                    atacante.Disputas++;
+                    oponente.Disputas++;
+
+                    d.Id = 0;//Zera o Id para poder salvar os dados de disputa sem erro de chave.
+                    d.DataDisputa = DateTime.Now;
+                    _context.Disputas.Add(d);
+                    await _context.SaveChangesAsync();
+                }
+
+                qtdPersonagensVivos = personagens.FindAll(p => p.Pontosvida > 0).Count;
+
+                if(qtdPersonagensVivos == 1)//Havendo só um personagem vivo, existe um CAMPEÃO!
+                {
+                    string resultadoFinal =
+                     $"{atacante.Nome.ToUpper()}é CAMPEÃO com {atacante.Pontosvida} pontos de vida!";
+                    
+                    d.Narracao += resultadoFinal;//Concatena o resultado final com as demais narrações.
+                    d.Resultados.Add(resultadoFinal);//Concaten o resultado final com os demais resultados.
+
+                    break;//break vai parar o while
+                }
+
+             }//Fim do while
+
+             //Código após o fechamento do while. Atualizará os pontos de vida, disputas, vitórias e derrotas de todos os personagens ao final das batalhas
+             _context.personagens.UpdateRange(personagens);
+             await _context.SaveChangesAsync();
+
+             return Ok (d);//retorna os dados da disputa 
+        }
+          catch(System.Exception ex)
+        {
+          return BadRequest(ex.Message);
+        }
+    }
+
+      [HttpDelete("ApagarDisputas")] 
+        public async Task<IActionResult> DeleteAsync()
+        {
+            try
+            {
+              List<Disputa> disputas = await _context.Disputas.ToListAsync();
+
+              _context.Disputas.RemoveRange(disputas);
+              await _context.SaveChangesAsync(); 
+
+              return Ok("Disputas apagadas"); 
+            }
+            catch (System.Exception ex)
+            {
+              return BadRequest(ex.Message);
+            }
+        }
+  
+
+    [HttpGet("Listar")]
+
+
+        public async Task<IActionResult> ListarAsync()
+        {
+            try
+            {
+                List<Disputa> disputas = await _context.Disputas.ToListAsync(); 
+                return Ok(disputas);
             }
             catch (System.Exception ex)
             {
--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -20,7 +20,7 @@ namespace RpgApi.Controllers
     [Authorize(Roles = "Jogador, Admin")]
     [ApiController]
     [Route("[Controller]")]
-    public class PersonagensController: ControllerBase
+    public class PersonagensController : ControllerBase
     {
         private readonly DataContext _context;
 
@@ -41,7 +41,7 @@ namespace RpgApi.Controllers
                     .Include(ar => ar.Arma)
                     .Include(us => us.Usuario)
                     .Include(ph => ph.PersonagemHabilidade)
-                        .ThenInclude(h => h.Habilidade)                    
+                        .ThenInclude(h => h.Habilidade)
                     .FirstOrDefaultAsync(pBusca => pBusca.Id == id);
 
                 return Ok(p);
@@ -52,38 +52,38 @@ namespace RpgApi.Controllers
             }
         }
 
-        [HttpGet ("GetAll")]
+        [HttpGet("GetAll")]
 
         public async Task<IActionResult> GetAll()
         {
             try
             {
-               List<personagem> lista = await _context.personagens.ToListAsync();
-                return Ok (lista);
+                List<personagem> lista = await _context.personagens.ToListAsync();
+                return Ok(lista);
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
-        }  
+        }
 
         [HttpPut]
         public async Task<IActionResult> Update(personagem novoPersonagem)
         {
             try
             {
-               if(novoPersonagem.Pontosvida > 100)
-               {
-                   throw new Exception ("Pontos de vida não poder ser maior que 100");
-               }
+                if (novoPersonagem.Pontosvida > 100)
+                {
+                    throw new Exception("Pontos de vida não poder ser maior que 100");
+                }
 
                 //int usuarioId = int.Parse(_httpContextAccessor.HttpContext.User.FindFirstOrDefault(ClaimTypes.NameIdentifier));
                 novoPersonagem.Usuario = _context.usuarios.FirstOrDefault(uBusca => uBusca.Id == ObterUsuarioId());
 
-               _context.personagens.Update(novoPersonagem);
-               int linhasAfetadas = await _context.SaveChangesAsync();
+                _context.personagens.Update(novoPersonagem);
+                int linhasAfetadas = await _context.SaveChangesAsync();
 
-               return Ok(linhasAfetadas);
+                return Ok(linhasAfetadas);
 
             }
             catch (Exception ex)
@@ -97,20 +97,20 @@ namespace RpgApi.Controllers
         {
             try
             {
-               if(novoPersonagem.Pontosvida > 100)
-               {
-                   throw new Exception ("Pontos de vida não poder ser maior que 100");
-               }
+                if (novoPersonagem.Pontosvida > 100)
+                {
+                    throw new Exception("Pontos de vida não poder ser maior que 100");
+                }
 
                 //int usuarioId = int.Parse(_httpContextAccessor.HttpContext.User.FirstOrDefault(ClaimTypes.NameIdentifier));
-                
-                
-               novoPersonagem.Usuario = _context.usuarios.FirstOrDefault(uBusca => uBusca.Id == ObterUsuarioId());
 
-               await _context.personagens.AddAsync(novoPersonagem);
-               await _context.SaveChangesAsync();
 
-               return Ok(novoPersonagem.Id);
+                novoPersonagem.Usuario = _context.usuarios.FirstOrDefault(uBusca => uBusca.Id == ObterUsuarioId());
+
+                await _context.personagens.AddAsync(novoPersonagem);
+                await _context.SaveChangesAsync();
+
+                return Ok(novoPersonagem.Id);
 
             }
             catch (Exception ex)
@@ -124,13 +124,13 @@ namespace RpgApi.Controllers
         {
             try
             {
-               personagem pRemover = await _context.personagens
-                .FirstOrDefaultAsync(p => p.Id == id);
+                personagem pRemover = await _context.personagens
+                 .FirstOrDefaultAsync(p => p.Id == id);
 
                 _context.personagens.Remove(pRemover);
                 int linhasAfetadas = await _context.SaveChangesAsync();
 
-               return Ok(linhasAfetadas);
+                return Ok(linhasAfetadas);
 
             }
             catch (Exception ex)
@@ -160,25 +160,25 @@ namespace RpgApi.Controllers
         }
 
 
-     [HttpGet("GetByUser")]
+        [HttpGet("GetByUser")]
 
-     public async Task<IActionResult> GetByUserAsync()
-     {
-        try
+        public async Task<IActionResult> GetByUserAsync()
         {
-            int id = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            try
+            {
+                int id = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
 
-            List<personagem> list = await _context.personagens
-                .Where(u => u.Usuario.Id == id).ToListAsync();
+                List<personagem> list = await _context.personagens
+                    .Where(u => u.Usuario.Id == id).ToListAsync();
 
                 return Ok(list);
-        }
-        catch (System.Exception ex)
-         {
-            return BadRequest(ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
 
-         }
-     }
+            }
+        }
 
         private int ObterUsuarioId()
         {
@@ -190,14 +190,14 @@ namespace RpgApi.Controllers
             return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Role);
         }
 
-         [HttpGet("GetByPerfil")]
+        [HttpGet("GetByPerfil")]
         public async Task<IActionResult> GetByPerfilAsync()
         {
             try
             {
                 List<personagem> lista = new List<personagem>();
 
-                if(ObterPerfilUsuario() == "Admin")
+                if (ObterPerfilUsuario() == "Admin")
                 {
                     lista = await _context.personagens.ToListAsync();
                 }
@@ -215,9 +215,127 @@ namespace RpgApi.Controllers
         }
 
 
+        [HttpPut("RestaurarPontosVida")]
+        public async Task<IActionResult> RestaurarPontosVidaAsync(personagem p)
+        {
+            try
+            {
+                int linhasAfetadas = 0;
+                personagem pEncontrado =
+                await _context.personagens.FirstOrDefaultAsync(pBusca => pBusca.Id == p.Id);
+                pEncontrado.Pontosvida = 100;
+
+                bool atualizou = await TryUpdateModelAsync<personagem>(pEncontrado, "p",
+                 pAtualizar => pAtualizar.Pontosvida);
+                // EF vai detectar e atualizar apenas as colunas que foram alteradas.
+                if (atualizou)
+                    linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut("AtualizarFoto")]
+        public async Task<IActionResult> AtualizarFoto(personagem p)
+
+        {
+            try
+            {
+                personagem personagem = await _context.personagens
+                    .FirstOrDefaultAsync(x => x.Id == p.Id);
+                personagem.FotoPersonagem = p.FotoPersonagem;
+                var attach = _context.Attach(personagem);
+                attach.Property(x => x.Id).IsModified = false;
+                attach.Property(x => x.FotoPersonagem).IsModified = true;
+                int linhasAfetadas = await _context.SaveChangesAsync();
+                return Ok(linhasAfetadas);
 
 
-    
+            }
+            catch (System.Exception ex)
+            { return BadRequest(ex.Message); }
+        }
+
+
+
+        [HttpPut("ZerarRanking")]
+        public async Task<IActionResult> ZerarRankingAsync(personagem p)
+        {
+            try
+            {
+                personagem pEncontrado =
+                    await _context.personagens.FirstOrDefaultAsync(pBusca => pBusca.Id == p.Id);
+
+                pEncontrado.Disputas = 0;
+                pEncontrado.Vitorias = 0;
+                pEncontrado.Derrotas = 0;
+                int linhasAfetadas = 0;
+
+                bool atualizou = await TryUpdateModelAsync<personagem>(pEncontrado, "p", pAtualizar => pAtualizar.Disputas, pAtualizar => pAtualizar.Vitorias, pAtualizar => pAtualizar.Derrotas); // EF vai detectar e atualizar apenas as colunas que foram alteradas.
+                if (atualizou)
+                    linhasAfetadas = await _context.SaveChangesAsync();
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("ZerarRankingRestaurarVidas")]
+        public async Task<IActionResult> ZerarRankingRestaurarVidasAsync()
+        {
+            try
+            {
+                List<personagem> lista = await _context.personagens.ToListAsync();
+                foreach (personagem p in lista)
+                {
+                    await ZerarRankingAsync(p);
+                    await RestaurarPontosVidaAsync(p);
+                }
+                return Ok();
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
 }
+
+
+
 
